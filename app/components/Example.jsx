5116595e22a1d4d658c547fe42b832127b5bcecd
Debanjan Ghosh <debanjang@ymail.com>
2018-11-13 17:38:07 +0530

    Centered all child components of main and added example locations to the Example page

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Example = React.createClass({
 //   render: function(){
@@ -15,8 +16,16 @@ var React = require('react');
 var Example = props =>{
   return(
     <div>
-      <h3> Example </h3>
-      <p> Welcome to the Examples page! </p>
+      <h1 className="text-center"> Examples </h1>
+      <p> Here are a few example locations for you to try out: </p>
+      <ol>
+        <li>
+          <Link to="/?location=Kolkata"> Kolkata, WB </Link>
+        </li>
+        <li>
+          <Link to="/?location=Toronto"> Toronto, Canada </Link>
+        </li>
+      </ol>
     </div>
   )
 }
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -17,8 +17,13 @@ var Main = props =>{
   return(
     <div>
       <Nav/>
-      <h2> Main Component </h2>
-      {props.children}
+      <div className="row" >
+        {/* On a medium screen take up 6 columns, on a large screen take up 4 columns and on a small screen take up all 12*/}
+        {/* Center the column using small centered. Foundation is mobile first, so using small automatically scales up to bigger screens*/}
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   )
 }
