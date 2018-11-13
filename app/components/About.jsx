0da911bc80331b95322d9686a9b153f061388f73
Debanjan Ghosh <debanjang@ymail.com>
2018-11-13 20:54:56 +0530

    Adding Style and content to the About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,10 +12,30 @@ var React = require('React');
 var About = props =>{
   return(
     <div>
-      <h3> About </h3>
-      <p> Welcome to the About page! </p>
+      <h1 className="text-center"> About </h1>
+      <p>
+        This is an application made with React that enables the user to search for
+        the temperature of any city in the world.
+       </p>
+       <p>
+        Here are some of the tools I used:
+       </p>
+      <ul>
+        <li>
+          <a href="https://reactjs.org" target="_blank"> React </a>
+          - This is the javascript library that forms the backbone of the application
+        </li>
+        <li>
+          <a href="https://openweathermap.org" target="_blank"> Open Weather Map </a>
+           - Open weather map provides the api that returns the temperature of the city that is searched
+        </li>
+        <li>
+          <a href="https://foundation.zurb.com" target="_blank"> Foundation </a>
+          - Foundation provides Style to the application
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
 module.exports = About;
