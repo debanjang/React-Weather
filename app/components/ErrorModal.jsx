9b58b6db0df116a11e6f6a1b665185fac8bf5d4a
Debanjan Ghosh <debanjang@ymail.com>
2018-11-21 00:17:09 +0530

    Fixed Error Modal Bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
   /*
@@ -7,10 +9,38 @@ var ErrorModal = React.createClass({
   ** Initialization that requires dom access,or calls to an external api with ajax.
   */
   componentDidMount: function(){
+    /* Since a third party should not alter the ReactDOM, we are going to use jQuery to create the 
+    DOM for this modal. We do that using $modal= $(ReactDOMServer.renderToString(modalMarkup)); 
+    to convert the markup into string and
+    then the $(ReactDOM.findDomNode(this)).html($modal); to wrap this component in jQuery selector 
+    and use the html method of jquery to render the modal in a seperate DOM. */ 
+    
+    var {title, message} = this.props;
+    
+    var modalMarkup = ( 
+      <div className="reveal tiny text-center" id="error-modal" data-reveal="">
+        <h4> {title} </h4>
+        <p> {message}! </p>
+        <p>
+          <button className="button hollow" data-close="">
+            Close
+          </button>
+        </p>
+      </div>
+    );
+
+    //Store the String representation of the markup in a variable
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    //Use Html to render this modal separately and not mess with the React DOM
+    //We use the ReactDOM to select a node to be wrapped in a jQuery Object and 
+    //then we use the html function ofthis object to create a new DOM.
+    $(ReactDOM.findDOMNode(this)).html($modal);
+    
     //create the modal
     var modal = new Foundation.Reveal($('#error-modal'));
     //show the modal
     modal.open();
+    
   },
 
   getDefaultProps: function(){
@@ -30,17 +60,10 @@ var ErrorModal = React.createClass({
   },
 
   render: function(){
-    var {title, message} = this.props;
+    /* We are returning an empty JSX here because we cant let Foundation 
+    alter React DOM. */
     return(
-      <div className="reveal tiny text-center" id="error-modal" data-reveal="">
-        <h4> {title} </h4>
-        <p> {message}! </p>
-        <p>
-          <button className="button hollow" data-close="">
-            Close
-          </button>
-        </p>
-      </div>
+     <div></div>
     );
   }
 
